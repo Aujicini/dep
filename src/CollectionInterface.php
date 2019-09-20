b429cd7cceff6bb2f6834c75a6d7f50b4e8befc4
oxuwazet <55375045+oxuwazet@users.noreply.github.com>
2019-09-20 12:50:23 -0400

    Update CollectionInterface.php

--- src/CollectionInterface.php
+++ src/CollectionInterface.php
@@ -1,48 +1,108 @@
-<?php declare(strict_types=1);
-/**
- * @author Oxuwazet.
- */
-
-namespace Oxuwazet\Collection;
+namespace Oxuwazet\DependencyInjection;
 
 /**
  * A collection object for managing variables more efficiently.
  *
- * @see <https://www.php.net/manual/en/class.arrayaccess.php>.
- *
- * @interface CollectionInterface.
+ * @class Collection.
  */
 interface CollectionInterface
 {
     /**
-     * Construct a new collection instance.
+     *  Construct a new collection.
      *
-     * @param array $insert Insert elements and values into the collection.
+     * @param array $insert Data to insert into the collection.
      *
      * @return void Returns nothing.
      */
-    public function __construct(array $insert);
+    public function __construct(array $insert)
+    {
+        foreach ($insert as $element => $value) {
+            $container[$element] = $value;
+        }
+    }
 
     /**
-     * Get the collection container.
+     * Get the containers instance.
      *
-     * @return array Returns the collection container.
+     * @return array Returns the containers instance.
      */
-    public function getContainer(): array;
+    public function getContainer(): array
+    {
+        return $this->container;
+    }
 
     /**
-     * Get the value associated with the element after unset the element.
+     * Check to see if the element exists.
+     *
+     * @param mixed $element The element for the value.
+     *
+     * @return bool Returns true if the element exists and false if not.
+     */
+    public function has($element): bool
+    {
+        return $this->offsetExists($element);
+    }
+
+    /**
+     * Remove a value.
+     *
+     * @param mixed $element The element for the value.
+     *
+     * @return void Returns nothing.
+     */
+    public function remove($element): void
+    {
+        $this->offsetUnset($element);
+    }
+
+    /**
+     * Get a value.
      *
      * @param mixed $element The element for the value.
      *
      * @return mixed Returns the value associated to the element.
      */
-    public function flash($element);
+    public function get($element)
+    {
+        return $this->offsetGet($element);
+    }
+
+    /**
+     * Insert a value.
+     *
+     * @param mixed $element The element for the value.
+     * @param mixed $value   The value associated to the element.
+     *
+     * @return void Returns nothing.
+     */
+    public function set($element, $value)
+    {
+        $this->offsetSet($element, $value);
+    }
+
+    /**
+     * Flash a value.
+     *
+     * @param mixed $element The element for the value.
+     *
+     * @return mixed Returns the value associated to the element.
+     */
+    public function flash($element)
+    {
+        $value = $this->offsetGet($element);
+        if ($this->offsetExists($element)) {
+            $this->offsetUnset($element);
+        }
+        return $value;
+    }
 
     /**
      * Clear the container.
      *
      * @return void Returns nothing.
      */
-    public function clear(): void;
+    public function clear(): void
+    {
+        $this->container = [];
+    }
 }
