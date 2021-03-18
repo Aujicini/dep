8f63a6562796a781bbd53e615fc8417aef533672
Nicholas English <oxuwazet@gmail.com>
2021-03-18 16:05:50 -0400

    Create psquaretable.h

---
+++ src/psquaretable.h
@@ -0,0 +1,26 @@
+#ifndef PSQUARETABLE_H
+#define PSQUARETABLE_H
+
+#include "defs.h"
+#include <vector>
+
+class Board;
+
+class PSquareTable {
+ public:
+  PSquareTable();
+  PSquareTable(const Board&);
+  static void init();
+  void addPiece(Color, PieceType, unsigned int);
+  void removePiece(Color, PieceType, unsigned int);
+  void movePiece(Color, PieceType, unsigned int, unsigned int);
+  int getScore(GamePhase, Color);
+ private:
+  static int PIECE_VALUES[2][2][6][64];
+  static void _setValues(std::vector<int>, PieceType, GamePhase);
+  static std::vector<int> _mirrorList(std::vector<int>);
+
+  int _scores[2][2] = {{0}};
+};
+
+#endif
